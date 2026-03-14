157661bfe5199f88d46d9f30feaf8cf7c69c3cd5
Pankaj Budhlakoti <126485766+PankajBudhlakoti910@users.noreply.github.com>
2026-03-14 04:48:29 +0000

    firebase firestore connected

--- dataforge/src/firebase/config.js
+++ dataforge/src/firebase/config.js
@@ -7,14 +7,14 @@ import { getAuth }        from 'firebase/auth'
 import { getAnalytics, isSupported } from 'firebase/analytics'
 
 const firebaseConfig = {
-  apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId:             import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId:     import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-}
+  apiKey: "AIzaSyDSAlOWtOm8lKFwUcaKbqr20lfE4x0f-qw",
+  authDomain: "dataforge-f3a63.firebaseapp.com",
+  projectId: "dataforge-f3a63",
+  storageBucket: "dataforge-f3a63.firebasestorage.app",
+  messagingSenderId: "89877466507",
+  appId: "1:89877466507:web:51009f1adb8a047344338e",
+  measurementId: "G-NHKRSCYYZ5"
+};
 
 // Validate config on startup
 const missingKeys = Object.entries(firebaseConfig)
